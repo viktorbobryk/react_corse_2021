4dcccc07d79f036dff575454d11b1812051abdf5
Viktor Bobryk <viktor.bobryk@gmail.com>
2021-07-21 19:03:02 +0300

    refactored styles

--- src/UIElements/index.jsx
+++ src/UIElements/index.jsx
@@ -1,6 +1,4 @@
-export { default as Input } from './Input';
 export { default as Logo } from './Logo';
 export { default as Loader } from './Loader';
 export { default as Tag, TAG_TYPE } from './Tag';
-export { default as TextArea } from './TextArea';
 export { default as Button, BUTTON_TYPE } from './Button';
